0828897e7d65e07fa8f296e870b0b3f09ea801fc
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-09-27 10:38:27 +1200

    referee and tests

--- verification/tests.py
+++ verification/tests.py
@@ -11,26 +11,94 @@ Each test is dict with
 TESTS = {
     "Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": "((5+3)*2+1)",
+            "answer": True
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
+            "input": "{[(3+1)+2]+}",
+            "answer": True
+        },
+        {
+            "input": "(3+{1-1)}",
+            "answer": False
+        },
+        {
+            "input": "[1+1]+(2*2)-{3/3}",
+            "answer": True
+        },
+        {
+            "input": "(({[(((1)-2)+3)-3]/3}-3)",
+            "answer": False
+        },
+        {
+            "input": "[(3)+(-1)]*{3}",
+            "answer": True
+        },
+        {
+            "input": "(((([[[{{{3}}}]]]]))))",
+            "answer": False
+        },
+        {
+            "input": "[1+202]*3*({4+3)}",
+            "answer": False
+        },
+        {
+
+            "input": "({[3]})-[4/(3*{1001-1000}*3)/4]",
+            "answer": True
+        },
+        {
+            "input": "[[[1+[1+1]]])",
+            "answer": False
+        },
+        {
+            "input": "(((1+(1+1))))]",
+            "answer": False
         }
+
     ],
     "Extra": [
         {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": "[[5+3]*2+1]",
+            "answer": True
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
+            "input": "{([3+1]+2)+}",
+            "answer": True
+        },
+        {
+            "input": "[3+{1-1]}",
+            "answer": False
+        },
+        {
+            "input": "(1+1)+[2*2]-{3/3}",
+            "answer": True
+        },
+        {
+            "input": "[[{([[[1]-2]+3]-3)/3}-3]",
+            "answer": False
+        },
+        {
+            "input": "([3]+[-1])*{3}",
+            "answer": True
+        },
+        {
+            "input": "[[[[((({{{3}}}))))]]]]",
+            "answer": False
+        },
+        {
+            "input": "(1+202)*3*[{4+3]}",
+            "answer": False
+        },
+        {
+
+            "input": "[{(3)}]-(4/[3*{1001-1000}*3]/4)",
+            "answer": True
+        },
+        {
+            "input": "(((1+(1+1))))]",
+            "answer": False
         }
+
     ]
 }
